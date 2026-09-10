0d0c656fdcd0bd39375b6e4245349d1bb01b2f4d
ZefiroTT <victorgamercna@gmail.com>
2026-09-10 23:18:50 +0000

    feat: adicionar insercao de novos registros

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import routerUsers from "./routes/user.route.ts";
 
 const app = express();
+
+app.use(express.json());
+
 app.use("/users", routerUsers);
 
 app.listen(3000);
